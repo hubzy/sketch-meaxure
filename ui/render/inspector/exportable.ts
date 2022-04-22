0a4fb15c58e84d9f0a9ca134e398ea5967aaa90f
hubzyy <zy.jpeg@gmail.com>
2022-04-22 23:49:33 +0800

    'pdf','tiff','eps'导出显示错误

--- ui/render/inspector/exportable.ts
+++ ui/render/inspector/exportable.ts
@@ -7,17 +7,25 @@ export function renderExportable(layerData: LayerData): string {
     expHTML.push('<ul class="exportable">');
     layerData.exportable.forEach(exportable => {
         var filePath = path + exportable.path;
-        expHTML.push('<li>', '<a href="' + filePath 
-        + '"target="_blank" download '
-        + 'data-format="' + exportable.format.toUpperCase() 
-        + '"><img class="dragExp" src="' + filePath + '" alt="' + exportable.path 
-        + '"><img class="thumbExp" src="' + filePath + '" alt="' + exportable.path 
-        + '">'
-        + exportable.path.replace('drawable-', '') 
-        + '</a>', '</li>');
+        if(['pdf','tiff','eps'].indexOf(exportable.format) == -1){
+            expHTML.push('<li>', '<a href="' + filePath
+            + '"target="_blank" download '
+            + 'data-format="' + exportable.format.toUpperCase()
+            + '"><img class="dragExp" src="' + filePath + '" alt="' + exportable.path
+            + '"><img class="thumbExp" src="' + filePath + '">'
+            + exportable.path.replace('drawable-', '')
+            + '</a>', '</li>');
+        }else{
+            expHTML.push('<li>', '<a href="' + filePath
+            + '"target="_blank" download '
+            + 'data-format="' + exportable.format.toUpperCase()
+            + '"><span style="color: #989A9C;margin-right: 2px;">[NotSupport]</span>'
+            + exportable.path.replace('drawable-', '')
+            + '</a>', '</li>');
+        }
     });
     expHTML.push('</ul>');
-    if(document.location.protocol == 'file:'){
+    if (document.location.protocol == 'file:') {
         expHTML.push('按(option)或(alt)点击即可下载')
     }
     return propertyType('EXPORTABLE', expHTML.join(''));
